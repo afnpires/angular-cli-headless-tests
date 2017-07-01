da6d8d7d434e3db1b6f735150b64744a169e9f9e
afnpires <afnpires@outlook.com>
2017-07-01 17:23:39 +0100

    add failing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,8 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
   }));
+
+  it('should fail', () => {
+    expect(1).toEqual(3);
+  });
 });
